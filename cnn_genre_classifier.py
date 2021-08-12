f6f1a4a330b2dd8bf82556f5aeea5332fe085e73
rmoser3 <rmoser3@students.kennesaw.edu>
2021-08-12 11:11:27 -0400

    Update
    - Added a new model
    - Moved models to their own file

--- cnn_genre_classifier.py
+++ cnn_genre_classifier.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
 import matplotlib.pyplot as plt
+import models
 
 DATA_PATH = "data.json"
 
@@ -80,53 +81,7 @@ def prepare_datasets(test_size, validation_size):
 
     return X_train, X_validation, X_test, y_train, y_validation, y_test
 
-def build_model(input_shape):
-    """Generates CNN model
-        :param input_shape (tuple): Shape of input set
-        :return model: CNN model
-    """
-    # create model
-    model = keras.Sequential()
 
-    # 1st conv layer
-        # the 1st parameter is # of filters.
-        # the 2nd is the grid size of the kernel.
-        # the 3rd is the activation function
-        # the 4th is the shape of the numpy array going into the layer
-    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=input_shape))
-
-    # 1st pooling layer
-        # the 1st parameter is the grid size.
-        # the 2nd is the (horizontal, vertical) stride.
-        # the 3rd is the padding. same means zero padding applied around ALL edges
-    model.add(keras.layers.MaxPool2D((3, 3), strides=(2,2), padding='same'))
-    # batch normalization is a process that normalizes the activation in the current layer and the subsequent layer
-        # it speeds up training. the models converge faster and the models are more reliable.
-        # its very complicated, go read about it
-    model.add(keras.layers.BatchNormalization())
-
-    # 2nd conv layer
-    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((3, 3), strides=(2,2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
-
-    # 3rd conv layer
-    model.add(keras.layers.Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
-
-    # flatten the output and feed it into dense layer
-    model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(64, activation='relu'))
-    model.add(keras.layers.Dropout(0.3))
-
-    # output layer
-        # the first parameter is the number of classes that can be classified.
-        # softmax creates scores for each of the 10 categories.
-        # the total for all 10 neurons = 1, the probability is distributed among them
-    model.add(keras.layers.Dense(10, activation='softmax'))
-
-    return model
 
 def predict(model, X, y):
     """Predict a single sample using the trained model
@@ -152,7 +107,7 @@ def run():
     # build the CNN net
     # X_train is a 4d array. see prepare_datasets
     input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
-    model = build_model(input_shape)
+    model = models.cnn_DS(input_shape=input_shape)
 
     # compile the network
     optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
---
+++ models.py
@@ -0,0 +1,109 @@
+import tensorflow.keras as keras
+
+# The model constructed by The Sound of AI
+    # https://www.youtube.com/watch?v=dOG-HxpbMSw
+def cnn_VV(input_shape):
+    """Generates CNN model
+        :param input_shape (tuple): Shape of input set
+        :return model: CNN model
+    """
+    # create model
+    model = keras.Sequential()
+
+    # 1st conv layer
+        # the 1st parameter is # of filters.
+        # the 2nd is the grid size of the kernel.
+        # the 3rd is the activation function
+        # the 4th is the shape of the numpy array going into the layer
+    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=input_shape))
+
+    # 1st pooling layer
+        # the 1st parameter is the grid size.
+        # the 2nd is the (horizontal, vertical) stride.
+        # the 3rd is the padding. same means zero padding applied around ALL edges
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2,2), padding='same'))
+    # batch normalization is a process that normalizes the activation in the current layer and the subsequent layer
+        # it speeds up training. the models converge faster and the models are more reliable.
+        # its very complicated, go read about it
+    model.add(keras.layers.BatchNormalization())
+
+    # 2nd conv layer
+    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2,2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # 3rd conv layer
+    model.add(keras.layers.Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # flatten the output and feed it into dense layer
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dropout(0.3))
+
+    # output layer
+        # the first parameter is the number of classes that can be classified.
+        # softmax creates scores for each of the 10 categories.
+        # the total for all 10 neurons = 1, the probability is distributed among them
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    return model
+
+def cnn_DS(input_shape):
+
+    model = keras.Sequential()
+
+    # X = Conv2D(128, kernel_size=(3, 3), strides=(1, 1))(X)
+    # X = BatchNormalization(axis=-1)(X)
+    # X = Activation('relu')(X)
+    # X = MaxPooling2D((2, 2))(X)
+
+    model.add(keras.layers.Conv2D(128, (3, 3), strides=(1, 1), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # X = Conv2D(64, kernel_size=(3, 3), strides=(1, 1))(X)
+    # X = BatchNormalization(axis=-1)(X)
+    # X = Activation('relu')(X)
+    # X = MaxPooling2D((2, 2))(X)
+
+    model.add(keras.layers.Conv2D(64, (3, 3), strides=(1, 1), activation='relu', input_shape=input_shape, padding="same"))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # X = Conv2D(32, kernel_size=(3, 3), strides=(1, 1)))(X)
+    # X = BatchNormalization(axis=3)(X)
+    # X = Activation('relu')(X)
+    # X = MaxPooling2D((2, 2))(X)
+
+    model.add(keras.layers.Conv2D(32, (3, 3), strides=(1, 1), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # X = Conv2D(16, kernel_size=(3, 3), strides=(1, 1))(X)
+    # X = BatchNormalization(axis=3)(X)
+    # X = Activation('relu')(X)
+    # X = MaxPooling2D((2, 2))(X)
+
+    model.add(keras.layers.Conv2D(16, (3, 3), strides=(1, 1), activation='relu', input_shape=input_shape, padding="same"))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # X = Conv2D(8, kernel_size=(3, 3), strides=(1, 1))(X_input)
+    # X = BatchNormalization(axis=3)(X)
+    # X = Activation('relu')(X)
+    # X = MaxPooling2D((2, 2))(X)
+
+    model.add(keras.layers.Conv2D(8, (3, 3), strides=(1,1), activation='relu', input_shape=input_shape, padding="same"))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    # X = Flatten()(X)
+    # X = Dropout(rate=0.3)
+    # X = Dense(classes, activation='softmax', name='fc' + str(classes))(X)
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    return model
